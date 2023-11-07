b2234c136aebbc39c90b9e68caebdcfd182bfe06
Henry <henryjrose2@gmail.com>
2023-11-07 08:44:21 -0500

    Third Benchmark - Concurrent 4 Threads

--- src/main/java/bench/MyBenchmark.java
+++ src/main/java/bench/MyBenchmark.java
@@ -9,23 +9,23 @@ import java.util.concurrent.TimeUnit;
 @Fork(value = 1, warmups = 1)
 public class MyBenchmark {
 
-    private seqMessageBoard messageBoard;
+    private parMessageBoard messageBoard;
 
     @Setup
     public void setup() {
         // Initialize the message board before each benchmark
-        messageBoard = new seqMessageBoard();
+        messageBoard = new parMessageBoard();
     }
 
     @Benchmark
-    @Threads(8) // Specify the number of threads for this benchmark
+    @Threads(4) // Specify the number of threads for this benchmark
     public void writeMessageBenchmark() {
         // Perform the writeMessage operation in your benchmark
         messageBoard.writeMessage("Sample message");
     }
 
     @Benchmark
-    @Threads(8) // Specify the number of threads for this benchmark
+    @Threads(4) // Specify the number of threads for this benchmark
     public void readMessagesBenchmark() {
         // Perform the readMessages operation in your benchmark
         messageBoard.readMessages();
--- src/main/java/bench/parMessageBoard.java
+++ src/main/java/bench/parMessageBoard.java
@@ -1,1 +1,23 @@
 package bench;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class parMessageBoard {
+    private Queue<String> messages;
+
+    public parMessageBoard() {
+        // Create a ConcurrentLinkedQueue for storing messages
+        messages = new ConcurrentLinkedQueue<>();
+    }
+
+    public void writeMessage(String message) {
+        // Add a message to the queue
+        messages.add(message);
+    }
+
+    public Object[] readMessages() {
+        // Retrieve all messages from the queue
+        return messages.toArray();
+    }
+}
